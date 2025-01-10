b3103953d9f15d7a5a60b2530890179ce1c08762
Matteo Cominetti <matteo@speckle.systems>
2025-01-10 21:31:06 +0100

    feat: remove arcgis page and menu item (#262)

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -143,7 +143,6 @@ module.exports = {
               ],
             },
             'qgis',
-            'arcgis',
             {
               title: 'Power BI',
               collapsable: true,
